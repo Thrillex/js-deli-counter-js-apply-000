a6b02f1a03ee960896bac9b04c2118c4c0630224
Ted Williams <tboxyz@gmail.com>
2019-07-18 03:43:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function nowServing(deliLine){
 function currentLine(deliLine){
     if (deliLine.length == 0){
   return("The line is currently empty.")
-} else { 
+} else {
   var list = `The line is currently:`
   deliLine.forEach(function(item,index,array)){
     var entry = ` ${index}. ${item},`;
@@ -29,4 +29,3 @@ function currentLine(deliLine){
 
 
 }
-
