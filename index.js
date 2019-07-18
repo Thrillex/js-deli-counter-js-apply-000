78b08ae734c2378cf8d8179e5ddee75c353a37a4
Ted Williams <tboxyz@gmail.com>
2019-07-18 03:19:19 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function takeANumber(deliLine,name){
+var count = deliLine.length+1;
+
+  deliLine.push(name);
+  return (`Welcome, ${name}. You are number ${count} in line.`);
+}
+
+function nowServing(deliLine){
+  var first = deliLine[0];
+  if (deliLine.length == 0){
+    return "There is nobody waiting to be served!";
+    }else {
+    return `Currently serving ${first}.`;}
+    deliLine.shift();
+
+}
+
+function currentLine(deliLine){
+    if (deliLine.length == 0){
+  return("The line is currently empty.")
+} else { for(var i=0; i < deliLine.length; i++){
+
+}
+
+
+
+}
+}
